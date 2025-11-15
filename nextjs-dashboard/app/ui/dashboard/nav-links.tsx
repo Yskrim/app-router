4815466a3f77743b88498914ac90c8cfa15c7a60
Yskrim <141317956+Yskrim@users.noreply.github.com>
2025-11-15 20:55:58 +1000

    chapter 4

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,19 +1,34 @@
+import { UserCircleIcon } from '@heroicons/react/20/solid';
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  UsersIcon,
 } from '@heroicons/react/24/outline';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { 
+    name: 'Home', 
+    href: '/dashboard', 
+    icon: HomeIcon 
+  },
   {
     name: 'Invoices',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { 
+    name: 'Customers', 
+    href: '/dashboard/customers', 
+    icon: UserGroupIcon 
+  },
+  // { 
+  //   name: 'New Link', 
+  //   href: '/dashboard/unknown', 
+  //   icon: UsersIcon
+  // },
 ];
 
 export default function NavLinks() {
