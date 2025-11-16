d7571775dd9743c57e33f6835ac7b11a5ca82cb9
Yskrim <141317956+Yskrim@users.noreply.github.com>
2025-11-16 23:06:04 +1000

    chapter-5: usePathname(), <Link/>

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { UserCircleIcon } from '@heroicons/react/20/solid';
 import {
   UserGroupIcon,
@@ -5,6 +6,10 @@ import {
   DocumentDuplicateIcon,
   UsersIcon,
 } from '@heroicons/react/24/outline';
+
+import clsx from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -37,14 +42,19 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                "bg-sky-100 text-blue-600" : usePathname() === link.href
+              }
+            )}
+            >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
